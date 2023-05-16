7ce1bf46890b7ee1ac08659a8600869e86942b42
Isaquehg <isaqueh.goncalves@gmail.com>
2023-05-16 17:04:03 -0300

    Real-time pizza graph implementation (only total data)

--- src/main/java/com/hackathon/flowwatcher/view/PizzaController.java
+++ src/main/java/com/hackathon/flowwatcher/view/PizzaController.java
@@ -1,6 +1,9 @@
 package com.hackathon.flowwatcher.view;
 
 import com.hackathon.flowwatcher.Application;
+import com.hackathon.flowwatcher.model.AppModel;
+import com.hackathon.flowwatcher.model.HostModel;
+import com.hackathon.flowwatcher.model.ProtocolModel;
 import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -15,9 +18,11 @@ import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
-public class PizzaController extends GraphController {
+public class PizzaController extends GraphController implements TrafficUIListener{
+    private static ObservableList<PieChart.Data> pieChartData;
 
     @FXML
     private PieChart pieChart;
@@ -45,5 +50,73 @@ public class PizzaController extends GraphController {
         stage.setScene(scene);
         stage.show();
     }
+
+    private void createPieChartAppDataRealTime() {
+        pieChartData = FXCollections.observableArrayList(
+                new PieChart.Data("Apples", 2),
+                new PieChart.Data("Oranges", 25),
+                new PieChart.Data("Grapes", 50),
+                new PieChart.Data("Melons", 3));
+
+        pieChartData.forEach(data ->
+                data.nameProperty().bind(
+                        Bindings.concat(data.getName(), " Total: ", data.pieValueProperty())
+                )
+        );
+
+        pieChart.setData(pieChartData);
+    }
+
+    // ------------------------------------REAL-TIME IMPLEMENTATION---------------------------------------
+    // Sending App usage to graph
+    @Override
+    public void onAppTrafficUpdated(List<AppModel> appModelList) {
+        // Cleaning previous data
+        pieChartData.clear();
+
+        // Create PieChart.Data objects using the appModelList
+        for (AppModel appModel : appModelList) {
+            PieChart.Data data = new PieChart.Data(appModel.getSource(), appModel.getTotal());
+            pieChartData.add(data);
+        }
+
+        // Update the pie chart data
+        pieChart.setData(pieChartData);
+    }
+
+    // Sending Data usage by Protocol to graph
+    @Override
+    public void onProtocolTrafficUpdated(List<ProtocolModel> protocolModelList) {
+        // Cleaning previous data
+        pieChartData.clear();
+
+        // Create PieChart.Data objects using the appModelList
+        for (ProtocolModel protocolModel : protocolModelList) {
+            PieChart.Data data = new PieChart.Data(protocolModel.getProtocol(), protocolModel.getTotal());
+            pieChartData.add(data);
+        }
+
+        // Update the pie chart data
+        pieChart.setData(pieChartData);
+    }
+
+    // Sending Data usage by Host to Graph
+    @Override
+    public void onHostTrafficUpdated(List<HostModel> hostModelList) {
+        // Cleaning previous data
+        pieChartData.clear();
+
+        // Create PieChart.Data objects using the appModelList
+        for (HostModel hostModel : hostModelList) {
+            PieChart.Data data = new PieChart.Data(hostModel.getSource(), hostModel.getTotal());
+            pieChartData.add(data);
+        }
+
+        // Update the pie chart data
+        pieChart.setData(pieChartData);
+    }
+
+    // ------------------------------------PAST DATA IMPLEMENTATION---------------------------------------
+
 
 }
