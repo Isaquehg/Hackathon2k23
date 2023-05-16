b034fba4a6b4f29fe83069b7d9c126eebb0a9061
Giv <giovanifinazzi@gmail.com>
2023-05-16 11:14:30 -0300

    Added download and upload buttons

--- src/main/java/com/hackathon/flowwatcher/view/GraphController.java
+++ src/main/java/com/hackathon/flowwatcher/view/GraphController.java
@@ -14,19 +14,29 @@ public class GraphController implements Initializable {
     @FXML
     private ChoiceBox<String> time;
     @FXML
+    private ChoiceBox<String> dataType;
+    @FXML
     private Label dataUsage;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         AddModeTimeData();
     }
     public void AddModeTimeData(){
+        // Add itens to checkBoxes
         mode.getItems().addAll("App", "Host", "Protocol");
         time.getItems().addAll("Realtime", "Last_24h", "Last_Week");
+        dataType.getItems().addAll("Download", "Upload");
+
+        // Set default value to checkBoxes and dataUsage
         mode.setValue("App");
         time.setValue("Realtime");
+        dataType.setValue("Download");
         dataUsage.setText("APP USAGE");
+
+        // Set the methods to checkBoxes
         mode.setOnAction(this::getParms);
         time.setOnAction(this::getParms);
+        dataType.setOnAction(this::getParms);
     }
 
     public void getParms(javafx.event.ActionEvent event) {
