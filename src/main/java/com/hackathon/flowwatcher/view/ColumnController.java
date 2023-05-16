835070567b3e48323e0a8937ba6063909c932f49
Giv <giovanifinazzi@gmail.com>
2023-05-15 22:47:34 -0300

    Front-end v1 finished!

--- src/main/java/com/hackathon/flowwatcher/view/ColumnController.java
+++ src/main/java/com/hackathon/flowwatcher/view/ColumnController.java
@@ -3,18 +3,28 @@ package com.hackathon.flowwatcher.view;
 import com.hackathon.flowwatcher.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.chart.StackedBarChart;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-
+import javafx.scene.control.Label;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ColumnController {
+public class ColumnController extends PizzaController implements Initializable {
     @FXML
     StackedBarChart stackedBarChart;
+    @FXML
+    private ChoiceBox<String> mode;
+    @FXML
+    private ChoiceBox<String> time;
     private Stage stage;
+    @FXML
+    private Label dataUsage;
 
     public void changeGraphic(MouseEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("pizza.fxml"));
@@ -22,5 +32,15 @@ public class ColumnController {
         Scene scene = new Scene(fxmlLoader.load(), 1366, 768);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        dataUsage.setText("APP USAGE");
+        mode.getItems().addAll("App", "Host", "Protocol");
+        time.getItems().addAll("Realtime", "Last_24h", "Last_Week");
+        mode.setValue("App");
+        time.setValue("Realtime");
+        mode.setOnAction(this::getParms);
     }
 }
